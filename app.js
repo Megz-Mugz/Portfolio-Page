f23e02d2ec3917c35dfff8214b5d41e91fd5cd5d
MegzMugz <rohan.mengle123@gmail.com>
2024-01-27 11:59:53 -0500

    decided to add typing animation to name

---
+++ app.js
@@ -0,0 +1,19 @@
+// Function to simulate typing animation
+function startTypingAnimation() {
+    const introText = document.getElementById("name");
+    const textToType = "Rohan Mengle";
+    let index = 0;
+
+    function typeCharacter() {
+        if (index < textToType.length) {
+            introText.textContent += textToType.charAt(index);
+            index++;
+            setTimeout(typeCharacter, 350); // Adjust typing speed here (milliseconds)
+        }
+    }
+
+    typeCharacter();
+}
+
+// Call the typing animation function when the page loads
+window.onload = startTypingAnimation;
